cce72f242e446c4144c30348326d3b50471a5e4f
pc <mohamedmouldi95@gmail.com>
2020-03-01 17:46:44 +0100

    first commit

---
+++ src/Entity/Produit.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\ProduitRepository")
+ */
+class Produit
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $nomprod;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $prix;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNomprod(): ?string
+    {
+        return $this->nomprod;
+    }
+
+    public function setNomprod(string $nomprod): self
+    {
+        $this->nomprod = $nomprod;
+
+        return $this;
+    }
+
+    public function getPrix(): ?int
+    {
+        return $this->prix;
+    }
+
+    public function setPrix(int $prix): self
+    {
+        $this->prix = $prix;
+
+        return $this;
+    }
+}
